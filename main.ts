b87c8f10d81db0f3cb3f94e4dbffaf4502a9d62a
darckfast <21337578+Darckfast@users.noreply.github.com>
2026-06-08 18:26:02 -0300

    feat: expose datacenter on response

--- main.ts
+++ main.ts
@@ -3,21 +3,21 @@ import app from "./app.wasm";
 import "./wasm_exec.js";
 
 globalThis.tryCatch = (o, fn, args) => {
-	try {
-		if (fn) {
-			return { data: o[fn](...args) };
-		}
+    try {
+        if (fn) {
+            return { data: o[fn](...args) };
+        }
 
-		return { data: o(...args) };
-	} catch (err) {
-		if (!(err instanceof Error)) {
-			if (err instanceof Object) {
-				err = JSON.stringify(err);
-			}
-			err = new Error(err || "no error message");
-		}
-		return { error: err };
-	}
+        return { data: o(...args) };
+    } catch (err) {
+        if (!(err instanceof Error)) {
+            if (err instanceof Object) {
+                err = JSON.stringify(err);
+            }
+            err = new Error(err || "no error message");
+        }
+        return { error: err };
+    }
 };
 
 let initiliazed = false;
@@ -26,34 +26,37 @@ let go = new Go();
 let instance = new WebAssembly.Instance(app, go.importObject);
 
 function init() {
-	if (!initiliazed) {
-		go.run(instance).finally(() => {
-			initiliazed = false;
-			instance = new WebAssembly.Instance(app, go.importObject);
-		});
-		initiliazed = true;
-	}
+    if (!initiliazed) {
+        go.run(instance).finally(() => {
+            initiliazed = false;
+            instance = new WebAssembly.Instance(app, go.importObject);
+        });
+        initiliazed = true;
+    }
 
-	if (go.exited) {
-		go = new Go();
-		go.run(instance).finally(() => {
-			instance = new WebAssembly.Instance(app, go.importObject);
-		});
-	}
+    if (go.exited) {
+        go = new Go();
+        go.run(instance).finally(() => {
+            instance = new WebAssembly.Instance(app, go.importObject);
+        });
+    }
 }
 
 export default class extends WorkerEntrypoint {
-	constructor(ctx, env) {
-		super(ctx, env);
+    constructor(ctx, env) {
+        super(ctx, env);
 
-		// Required to init tne `env` and `ctx` variables
-		// and populate this class's prototype with RPC stubs
-		globalThis.workerapp = this;
-		init();
-	}
+        // Required to init tne `env` and `ctx` variables
+        // and populate this class's prototype with RPC stubs
+        globalThis.workerapp = this;
+        init();
+    }
 
-	async fetch(request: Request): Response | Promise<Response> {
-		return await cf.fetch(request, this.env, this.ctx);
-	}
+    async fetch(request: Request): Response | Promise<Response> {
+        let response = await cf.fetch(request, this.env, this.ctx) as Response;
+        response.headers.set('x-datacenter', request.cf?.colo)
+
+        return response
+    }
 }
 
